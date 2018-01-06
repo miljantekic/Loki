8091a6bd208c0ccef50bb55ecfb1565b2fe62a9c
Miljan Tekic <mtekic@godaddy.com>
2018-01-06 17:10:32 +0100

    Created basic method for fetching endpoints and responses

--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,79 @@
 var fs = require('fs');
+var _ = require('lodash');
+var express = require('express');
 
 var defaultConfig = {
+    port: 1337,
+    serverSuffix: '/mock',
     resourcesPath: process.cwd() + '/resources/loki/'
 };
 
 var ENDPOINTS_DIR = 'endpoints';
 var RESPONSES_DIR = 'responses';
 
-module.exports.startServer = function () {
-    var path = defaultConfig.resourcesPath + ENDPOINTS_DIR;
+function findEndpoint(url, method, config) {
+    var path = config.resourcesPath + ENDPOINTS_DIR;
 
-    fs.readdir(path, function(err, items) {
-        console.log(items);
+    var endpointsFiles = fs.readdirSync(path);
 
-        for (var i=0; i<items.length; i++) {
-            console.log(JSON.parse(fs.readFileSync(path + '/' + items[i], 'utf8')));
-        }
+    var endpoints = _.map(endpointsFiles, function (endpointFile) {
+        return JSON.parse(fs.readFileSync(path + '/' + endpointFile, 'utf8'))
     });
 
-    console.log('Server has been started', path);
+    var endpoint = _.find(endpoints, {
+        url: url,
+        method: method
+    });
+
+    if (!endpoint) {
+        return null;
+    }
+
+    return endpoint;
+}
+
+function findResponse(responseId, config) {
+    var responseFilePath = config.resourcesPath + RESPONSES_DIR + '/' + responseId + '.json';
+    var responseFileContent = '';
+
+    try {
+        responseFileContent = fs.readFileSync(responseFilePath, 'utf8');
+    } catch (e) {
+        return null;
+    }
+
+    return JSON.parse(responseFileContent);
+}
+
+function setupServer(server, config) {
+    server.use(config.serverSuffix + '/*', function(req, res){
+        var endpoint = findEndpoint(req.originalUrl.replace(config.serverSuffix, ''), req.method, config);
+
+        if (!endpoint) {
+            res.status(404).send('Sorry, that endpoint does not exist!');
+
+            return;
+        }
+
+        var response = findResponse(endpoint.activeResponse, config);
+
+        if (!response) {
+            res.status(404).send('Could not find response content!');
+
+            return;
+        }
+
+        res.status(response.statusCode).send(response.responseBody);
+    });
+}
+
+
+module.exports.startServer = function () {
+    var server = express();
+
+    setupServer(server, defaultConfig);
+
+    server.listen(defaultConfig.port, function () {
+        console.log('Server has been started on port ' + defaultConfig.port + '');
+    });
 };
