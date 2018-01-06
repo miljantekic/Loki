417d65716aae07599804e9a4d9b698300004eb01
Miljan Tekic <mtekic@godaddy.com>
2018-01-06 17:26:34 +0100

    Integrated models in the endpoint service

--- src/server/Endpoints.js
+++ src/server/Endpoints.js
@@ -1,16 +1,23 @@
 var fs = require('fs');
 var _ = require('lodash');
+var Endpoint = require('./models/Endpoint');
 
 var Endpoints = {};
 var ENDPOINTS_DIR = 'endpoints';
 
+/**
+ * @param {string} url
+ * @param {string} method
+ * @param {object} config
+ * @returns {null | Endpoint}
+ */
 Endpoints.findEndpoint = function(url, method, config) {
     var path = config.resourcesPath + ENDPOINTS_DIR;
 
     var endpointsFiles = fs.readdirSync(path);
 
     var endpoints = _.map(endpointsFiles, function (endpointFile) {
-        return JSON.parse(fs.readFileSync(path + '/' + endpointFile, 'utf8'))
+        return new Endpoint(JSON.parse(fs.readFileSync(path + '/' + endpointFile, 'utf8')));
     });
 
     var endpoint = _.find(endpoints, {
