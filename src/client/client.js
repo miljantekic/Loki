cc6a08792e58b94697068ae25a2a4c6bc8ae9033
Miljan Tekic <mtekic@godaddy.com>
2018-01-07 22:08:38 +0100

    Added serving for static application

--- src/client/client.js
+++ src/client/client.js
@@ -1,3 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
 module.exports.renderApplication = function () {
-    console.log('Application has been rendered');
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+
+    app.listen(9000, function () {
+        console.log('Application has been rendered');
+    });
 };
